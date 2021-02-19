b76c26108be0649dbd036ba7872b3332e2bce9bb
Jin <mjin1789@gmail.com>
2021-02-19 13:33:35 +0900

    hash with out recursion

--- hash.py
+++ hash.py
@@ -1,26 +1,8 @@
-
-class Find_mod:
-    def __init__(self):
-        self.size = int(input())
-        self.strings = list(input())
-        self.mod_num = 1234567891
-        self.sum = 0
-    
-    def hash(self,num,index):
-        if index == self.size:
-            print(self.sum)
-            return
-
-        if num > self.mod_num:
-            self.sum += (num * (ord(self.strings[index]) - 96)) % self.mod_num
-
-        else:
-            self.sum += num * (ord(self.strings[index]) - 96)
-
-        num = num * 31
-        index += 1
-        self.hash(num,index)
-    
-if __name__ == "__main__":
-    h = Find_mod()
-    h.hash(1,0)
+size = int(input())
+strigns = list(input())
+val = 0
+sum = 0
+for i in range(size):
+    val = ((ord(strigns[i])-96)*pow(31,i))
+    sum += val
+print(sum % 1234567891)
