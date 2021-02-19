f19fd103673148ad74279ae58f50cd4c95466d33
Jin <mjin1789@gmail.com>
2021-02-19 16:45:54 +0900

    fix hash collison by using chaining and success

--- find_num.py
+++ find_num.py
@@ -1,14 +1,23 @@
-table = [0 for i in range(0,100000)]
+table = [[] for i in range(0,100000)]
 n = input()
 keys = list(map(int,input().split()))
 m = input()
 compare = list(map(int,input().split()))
 
+
 for k in keys:
     idx = k % len(table)
-    if not table[idx]:
-        table[idx] = 1
+    table[idx].append(k)
 
 for c in compare:
     idx = c % len(table)
-    print(table[idx])
+    find = False
+    for val in table[idx]:
+        if val == c:
+            # print("idx: {0} val: {1}".format(idx,val))
+            print(1)
+            find = True
+            break
+
+    if not find:
+        print(0)
