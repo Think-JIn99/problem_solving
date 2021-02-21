733aaac9abc77161884b96c385d67282cbc167f8
Jin <mjin1789@gmail.com>
2021-02-22 00:45:29 +0900

    Baek joon 1786 changing way not using regex method

--- regex.py
+++ regex.py
@@ -1,15 +1,20 @@
 import re
 import sys
-# sys.stdin.readline()
-texts = "aaaaaaaaa"
-pattern = "aa"
-match_pattern = re.finditer(pattern,texts)
+# texts = sys.stdin.readline()
+# pattern = sys.stdin.readline()
+texts = 'banana banana'
+pattern = 'ana'
+match_pattern = re.finditer(pattern[0],texts)
 res = ""
 length = 0
 for m in match_pattern:
-    res += "{0} ".format(m.start() + 1)
-    length += 1
-
+    start = m.start() + 1
+    lasts = texts[start:(start - 1 + len(pattern))]
+    if lasts != '':
+        temp = re.match(pattern[1:],lasts)
+        if temp:
+            res += "{0} ".format(start)
+            length += 1
 print(length)
 print(res)
 
