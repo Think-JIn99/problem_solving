439698b9bded161a14882c2aea13f3c8df94bff3
Jin <mjin1789@gmail.com>
2021-02-21 23:53:44 +0900

    baek joon 1786 with regex no success

--- regex.py
+++ regex.py
@@ -1,6 +1,15 @@
 import re
-strings = "ABC ABCDAB ABCDABCDABDE"
-match_pattern = "ABCDABD"
-print(len(re.findall(match_pattern,strings)))
-for matched in re.finditer(match_pattern,strings):
-    print(matched.start() + 1)
+import sys
+# sys.stdin.readline()
+texts = "aaaaaaaaa"
+pattern = "aa"
+match_pattern = re.finditer(pattern,texts)
+res = ""
+length = 0
+for m in match_pattern:
+    res += "{0} ".format(m.start() + 1)
+    length += 1
+
+print(length)
+print(res)
+
