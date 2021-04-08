88fd19165ffea51f600f60600935f988cf22e8f7
Jin <mjin1789@gmail.com>
2021-04-08 11:46:26 +0900

    백준 통과 완성본

--- permutaition.py
+++ permutaition.py
@@ -1,13 +1,13 @@
 import copy
-def permutaition(num_list,remved_num):
+def permutaition(num_list,removed_nums):
     if len(num_list) == 1:
-        print(f"{remved_num}{num_list}", end = '')
+        print(removed_nums + num_list)
         return
 
     for i in num_list:
         list_copy = copy.deepcopy(num_list)
-        permutaition(list_copy.remove(i),remved_num + i)
-        print('')
+        permutaition(list_copy.replace(i,''), removed_nums + i + " ")
 
-num_list = int(input())
-permutaition(list(range(1,num_list + 1)),0)
+num_list = [str(i) for i in range(1 , int(input()) + 1)]
+num_list = ''.join(num_list)
+permutaition(num_list,"")
